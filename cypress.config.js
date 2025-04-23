17d1eb928f476c54ddcb2b3714294a0601c41d5f
Nitesh Khatiwoda <nitesh@danpheit.com>
2025-04-23 13:43:57 +0545

    Revert "env fixes"
    This reverts commit 0a5042b78e511206e3ca59e76f548b40a0690ed3.

--- cypress.config.js
+++ cypress.config.js
@@ -12,38 +12,14 @@
 // });
 
 
-// // cypress.config.js
-// require('dotenv').config({ path: '.env.ms' }); // <-- Add this line
-
-// const { defineConfig } = require("cypress");
-
-// module.exports = defineConfig({
-//   e2e: {
-//     baseUrl: "https://meroshare.cdsc.com.np",
-//     env: {
-//       USER_NAME: process.env.USER_NAME,
-//       PASSWORD: process.env.PASSWORD,
-//       DP: process.env.DP,
-//       MAX_IPO_PRICE: process.env.MAX_IPO_PRICE,
-//       KITTA: process.env.KITTA,
-//       CRN: process.env.CRN,
-//       TRANSACTION_PIN: process.env.TRANSACTION_PIN,
-//       BANK_NAME: process.env.BANK_NAME,
-//       TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
-//       TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID
-//     },
-//     setupNodeEvents(on, config) {
-//       return config;
-//     }
-//   }
-// });
-
-require('dotenv').config({ path: '.env' }); // <--- this is important
+// cypress.config.js
+require('dotenv').config({ path: '.env.ms' }); // <-- Add this line
 
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://meroshare.cdsc.com.np",
     env: {
       USER_NAME: process.env.USER_NAME,
       PASSWORD: process.env.PASSWORD,
@@ -58,7 +34,7 @@ module.exports = defineConfig({
     },
     setupNodeEvents(on, config) {
       return config;
-    },
-  },
+    }
+  }
 });
 
